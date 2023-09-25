505154595e79dd60046b1420dc7d9d6691248bd6
Jordano Iqbal Darmawan <79011617+jordanoid@users.noreply.github.com>
2023-09-25 08:49:31 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import State from "../src/pages/learn-useState/index";
+import Effect from "../src/pages/learn-useEffect/index";
+import Context from "../src/pages/learn-useContext/index";
+
+function App() {
+	return (
+		<>
+			<Router>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/use-state">Use State</Link>
+						</li>
+						<li>
+							<Link to="/use-effect">Use Effect</Link>
+						</li>
+						<li>
+							<Link to="/use-context">Use Context</Link>
+						</li>
+					</ul>
+				</nav>
+				<Routes>
+					<Route path="/" exact element={<State />}></Route>
+					<Route path="/use-state" exact element={<State />}></Route>
+					<Route path="/use-effect" exact element={<Effect />}></Route>
+					<Route path="/use-context" exact element={<Context />}></Route>
+				</Routes>
+			</Router>
+		</>
+	);
+}
+
+export default App;
